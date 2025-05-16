a0f0a6d057428a43f772fe0d8a2641bddfb4af57
elancheran-chandran <dmk.elancheran@gmail.com>
2025-05-16 14:12:54 +0530

    ADD FSD Program Tool Details

--- readme.js
+++ readme.js
@@ -2,8 +2,12 @@
 Day-1 Class
 
 Tools required for FSD Program
-    - VS Code
-    - Github Accout
-    - Git
-
+    - VS Code : https://code.visualstudio.com/docs/?dv=win64user
+    - Github Accout : https://github.com/ 
+    - Git : https://git-scm.com/downloads/win
+    - Postman : https://www.postman.com/downloads/
+    - Nodejs : https://nodejs.org/en/download/
+    - npm (Not nessesary ,inbuilt from nodejs)
+    - Browser : Safari,Chrome,Firefox
+    - Netlify : https://www.netlify.com/
 */
